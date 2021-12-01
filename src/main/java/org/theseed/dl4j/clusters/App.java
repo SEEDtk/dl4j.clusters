69ef3dccc99414195b835a2284f6947016320ddd
Bruce Parrello <bparrello@figresearch.com>
2021-12-01 06:13:52 -0600

    First version.

---
+++ src/main/java/org/theseed/dl4j/clusters/App.java
@@ -0,0 +1,30 @@
+package org.theseed.dl4j.clusters;
+
+import java.util.Arrays;
+
+import org.theseed.utils.BaseProcessor;
+
+public class App {
+    
+    public static void main( String[] args )
+    {
+        // Get the control parameter.
+        String command = args[0];
+        String[] newArgs = Arrays.copyOfRange(args, 1, args.length);
+        BaseProcessor processor;
+        // Determine the command to process.
+        switch (command) {
+        case "cluster" :
+            processor = new ClusterProcessor();
+            break;
+        default:
+            throw new RuntimeException("Invalid command " + command);
+        }
+        // Process it.
+        boolean ok = processor.parseCommand(newArgs);
+        if (ok) {
+            processor.run();
+        }
+    }
+    
+}
---
+++ src/test/java/org/theseed/dl4j/clusters/AppTest.java
@@ -0,0 +1,13 @@
+package org.theseed.dl4j.clusters;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+}
